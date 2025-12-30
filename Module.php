4dd732ce7e521c88bfa697408a1d2b8162ca61a9
Saumyajit <saumyajit@users.noreply.github.com>
2025-12-30 21:03:08 +0530

    Update Module.php

--- Module.php
+++ Module.php
@@ -11,14 +11,9 @@ class Module extends CModule {
             ->findOrAdd(_('Reports'))
                 ->getSubmenu();
         
-        // Keep existing Disk Analyser
-        $menu->insertAfter(_('Notification'),
-            (new CMenuItem(_('Disk Analyser')))->setAction('disk.analyser')
-        );
-        
         // Add new Storage Analytics menu item
-        $menu->insertAfter(_('Disk Analyser'),
+        $menu->insertAfter(_('Notification'),
             (new CMenuItem(_('Storage Analytics')))->setAction('storage.analytics')
-        );
+        );        
     }
 }
