9356918a286bd88ea837891e5029a61d7bfd3aa3
Saumyajit <saumyajit@users.noreply.github.com>
2025-12-30 15:46:58 +0530

    Increase limit from 1000 to 13000 in StorageAnalytics

--- actions/StorageAnalytics.php
+++ actions/StorageAnalytics.php
@@ -329,7 +329,7 @@ class StorageAnalytics extends CController {
 				'history'   => 0,              // numeric float (pused is numeric)
 				'time_from' => $timeFrom,
 				'sortfield' => ['itemid', 'clock'],
-				'limit'     => 1000
+				'limit'     => 13000
 			]);
 	
 			// Group history by item ID
